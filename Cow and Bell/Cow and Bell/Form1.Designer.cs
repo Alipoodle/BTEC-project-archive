5da10d4ff89d5527cd11d622dfec6586a02191ee
Alipoodle M <ali.poodle12@gmail.com>
2018-09-14 11:23:41 +0100

    Cow and Bell Upload

---
+++ Cow and Bell/Cow and Bell/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace Cow_and_Bell
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblCowBell = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblGuess = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 33);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(20, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(38, 33);
+            this.textBox2.MaxLength = 1;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(20, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(64, 33);
+            this.textBox3.MaxLength = 1;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(20, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(90, 33);
+            this.textBox4.MaxLength = 1;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(20, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // lblCowBell
+            // 
+            this.lblCowBell.AutoSize = true;
+            this.lblCowBell.Location = new System.Drawing.Point(136, 9);
+            this.lblCowBell.Name = "lblCowBell";
+            this.lblCowBell.Size = new System.Drawing.Size(76, 13);
+            this.lblCowBell.TabIndex = 5;
+            this.lblCowBell.Text = "0 Cows 0 Bells";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(139, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblGuess
+            // 
+            this.lblGuess.AutoSize = true;
+            this.lblGuess.Location = new System.Drawing.Point(12, 9);
+            this.lblGuess.Name = "lblGuess";
+            this.lblGuess.Size = new System.Drawing.Size(49, 13);
+            this.lblGuess.TabIndex = 7;
+            this.lblGuess.Text = "Guess: 0";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(224, 66);
+            this.Controls.Add(this.lblGuess);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblCowBell);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCowBell;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblGuess;
+    }
+}
+
---
+++ Cow and Bell/Cow and Bell/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Cow_and_Bell
+{
+    public partial class Form1 : Form
+    {
+        int[] pubNumbers;
+        int guessCount = 0;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int[] genNumber()
+        {
+
+            Random random = new Random();
+            int d1 = random.Next(0, 9);
+            int d2 = random.Next(0, 9);
+            int d3 = random.Next(0, 9);
+            int d4 = random.Next(0, 9);
+
+            int[] numbers = new int[4];
+            if (d1 != d2 && d1 != d3 && d1 != d4 && d2 != d3 && d2 != d4 && d3 != d4)
+            {
+                
+                numbers[0] = d1;
+                numbers[1] = d2;
+                numbers[2] = d3;
+                numbers[3] = d4;
+                return numbers;
+            }
+            else
+            {
+                return genNumber();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pubNumbers = genNumber();
+            string result = string.Join("", pubNumbers);
+            label1.Text = result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            guessCount++;
+            int[] guessNum = new int[4];
+            try
+            {
+                guessNum[0] = Int32.Parse(textBox1.Text);
+                guessNum[1] = Int32.Parse(textBox2.Text);
+                guessNum[2] = Int32.Parse(textBox3.Text);
+                guessNum[3] = Int32.Parse(textBox4.Text);
+            }
+            catch
+            {
+                MessageBox.Show("Please only use numbers!");
+            }
+
+            int cow = 0;
+            int bell = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                for (int o = 0; o < 4; o++)
+                {
+                    if (pubNumbers[i] == guessNum[o])
+                    {
+                        if (i == o)
+                        {
+                            cow++;
+                        }
+                        else
+                        {
+                            bell++;
+                        }
+                    }
+                    
+                }
+            }
+            lblCowBell.Text = string.Format("{0} Cow, {1} Bells", cow, bell);
+            lblGuess.Text = string.Format("Guess: {0}", guessCount);
+            if (cow == 4)
+            {
+                MessageBox.Show("You Won!");
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+                textBox4.Text = "";
+                lblGuess.Text = "Guess: 0";
+                guessCount = 0;
+                lblCowBell.Text = "0 Cow, 0 Bells";
+                cow = 0;
+                bell = 0;
+
+                pubNumbers = genNumber();
+                string result = string.Join("", pubNumbers);
+                label1.Text = result;
+            }
+
+        }
+    }
+}
