0ad8f22aa82645c6f004d945c07edc35ef8a1af4
Alipoodle M <ali.poodle12@gmail.com>
2018-09-14 12:01:54 +0100

    Car Sales

---
+++ Car sales/WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.cbCarNames = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.lblTotalSales = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblNumberSold = new System.Windows.Forms.Label();
+            this.lblSoldCars = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(3, 1);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(281, 165);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.label6);
+            this.tabPage1.Controls.Add(this.cbCarNames);
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(273, 139);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Add Sold Car";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(190, 7);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(8, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Car Name";
+            // 
+            // cbCarNames
+            // 
+            this.cbCarNames.FormattingEnabled = true;
+            this.cbCarNames.Items.AddRange(new object[] {
+            "Mercedes-Benz Cla Class CLA 250 AMG 4dr Tip Auto 2.0",
+            "Hyundai Coupe 1.6i S 3dr",
+            "Citroen C4 1.6 HDi 16v VTR 3dr",
+            "BMW 3 SERIES 318Ci SE 2.0 2dr",
+            "Hyundai Coupe 1.6 S 3dr",
+            "Toyota Celica 1.8 VVTI 3dr",
+            "Mercedes-Benz C Class 2.5 C230 SE 7G-Tronic 2dr"});
+            this.cbCarNames.Location = new System.Drawing.Point(11, 53);
+            this.cbCarNames.Name = "cbCarNames";
+            this.cbCarNames.Size = new System.Drawing.Size(256, 21);
+            this.cbCarNames.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(7, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Add Sold Car";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.lblTotalSales);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.lblNumberSold);
+            this.tabPage2.Controls.Add(this.lblSoldCars);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.label3);
+            this.tabPage2.Controls.Add(this.button1);
+            this.tabPage2.Controls.Add(this.label2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(273, 139);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "View Car Sales";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            this.tabPage2.Click += new System.EventHandler(this.tabPage2_Click);
+            // 
+            // lblTotalSales
+            // 
+            this.lblTotalSales.AutoSize = true;
+            this.lblTotalSales.Location = new System.Drawing.Point(90, 40);
+            this.lblTotalSales.Name = "lblTotalSales";
+            this.lblTotalSales.Size = new System.Drawing.Size(81, 13);
+            this.lblTotalSales.TabIndex = 8;
+            this.lblTotalSales.Text = "(Please update)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(22, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(63, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Total Sales:";
+            // 
+            // lblNumberSold
+            // 
+            this.lblNumberSold.AutoSize = true;
+            this.lblNumberSold.Location = new System.Drawing.Point(90, 105);
+            this.lblNumberSold.Name = "lblNumberSold";
+            this.lblNumberSold.Size = new System.Drawing.Size(81, 13);
+            this.lblNumberSold.TabIndex = 6;
+            this.lblNumberSold.Text = "(Please update)";
+            // 
+            // lblSoldCars
+            // 
+            this.lblSoldCars.AutoSize = true;
+            this.lblSoldCars.Location = new System.Drawing.Point(90, 84);
+            this.lblSoldCars.Name = "lblSoldCars";
+            this.lblSoldCars.Size = new System.Drawing.Size(81, 13);
+            this.lblSoldCars.TabIndex = 5;
+            this.lblSoldCars.Text = "(Please update)";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 106);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Number Sold:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Most Sold Car:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(190, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(5, 6);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "View Car Sales";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 168);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblNumberSold;
+        private System.Windows.Forms.Label lblSoldCars;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblTotalSales;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox cbCarNames;
+    }
+}
+
---
+++ Car sales/WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        string[] lines;
+        string fileName = System.Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + @"\carsales.txt";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (!File.Exists(fileName))
+            {
+                fileLoad();
+                return;
+            } else
+            {
+                fileLoad();
+            }
+            if (lines.Length != 0)
+            {
+                var mode = lines.GroupBy(v => v).OrderByDescending(g => g.Count()).First();
+
+                lblSoldCars.Text = mode.Key;
+                lblNumberSold.Text = mode.Count().ToString();
+                lblTotalSales.Text = lines.Length.ToString();
+            } else
+            {
+                MessageBox.Show("Error it seems like there's no content in the file");
+            }
+        }
+
+        public void fileLoad()
+        {
+            if (File.Exists(fileName))
+            {
+                lines = File.ReadAllLines(fileName);
+            }
+            else {
+                MessageBox.Show("Failed to find file! Creating file at location: " + fileName);
+                File.Create(fileName);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // This text is always added, making the file longer over time
+            // if it is not deleted.
+            using (StreamWriter sw = File.AppendText(fileName))
+            {
+                sw.WriteLine(cbCarNames.Text);
+            }
+            MessageBox.Show($"Successfully added {cbCarNames.Text} to list");
+        }
+    }
+}
